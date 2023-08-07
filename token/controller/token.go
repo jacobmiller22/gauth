5fb78ece15253733e48900fedb3f8eaf459361d8
Jacob Miller <dev@jacobmiller22.com>
2023-08-06 22:25:16 -0400

    error handling for token response

--- token/controller/token.go
+++ token/controller/token.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 
@@ -16,9 +15,44 @@ type TokenResponse struct {
 	ExpiresIn   int64  `json:"expires_in"`
 }
 
+type TokenError int32
+
+const (
+	InvalidRequest = iota
+	InvalidClient
+	InvalidGrant
+	InvalidScope
+	UnauthorizedClient
+	UnsupportedGrantType
+)
+
+func (e TokenError) String() string {
+	switch e {
+	case InvalidRequest:
+		return "invalid_request"
+	case InvalidClient:
+		return "invalid_client"
+	case InvalidGrant:
+		return "invalid_grant"
+	case InvalidScope:
+		return "invalid_scope"
+	case UnauthorizedClient:
+		return "unauthorized_client"
+	case UnsupportedGrantType:
+		return "unsupported_grant_type"
+	}
+	return ""
+}
+
+type TokenErrorResponse struct {
+	Error            string `json:"error"`
+	ErrorDescription string `json:"error_description"`
+	ErrorUri         string `json:"error_uri"`
+}
+
 func Token(c *gin.Context) {
 	code := c.Query("code")
-	// grantType := c.Query("fsdfgrant_type")
+	grantType := c.Query("grant_type")
 	clientId := c.Query("client_id")
 	clientSecret := c.Query("client_secret")
 	redirectUri := c.Query("redirect_uri")
@@ -26,18 +60,32 @@ func Token(c *gin.Context) {
 
 	authReq, err := authService.GetAuthReqByCode(code)
 	if err != nil {
-		fmt.Printf("error getting authreq: %+v\n", err)
-		c.JSON(http.StatusBadRequest, 0)
+		c.JSON(http.StatusBadRequest, TokenErrorResponse{
+			Error:            TokenError.String(InvalidRequest),
+			ErrorDescription: "Could not find auth code.",
+		})
 		return
 	}
-	fmt.Printf("authReq: %+v\n", authReq)
+	if grantType == "client_credientials" {
+		c.JSON(http.StatusBadRequest, TokenErrorResponse{
+			Error:            TokenError.String(InvalidGrant),
+			ErrorDescription: "Invalid grant_type",
+		})
+		return
+	}
 	if clientId != authReq.ClientId {
-		c.JSON(http.StatusBadRequest, 0)
+		c.JSON(http.StatusBadRequest, TokenErrorResponse{
+			Error:            TokenError.String(InvalidClient),
+			ErrorDescription: "Invalid client_id",
+		})
 		return
 	}
 
 	if redirectUri != authReq.RedirectUri {
-		c.JSON(http.StatusBadRequest, 0)
+		c.JSON(http.StatusBadRequest, TokenErrorResponse{
+			Error:            TokenError.String(InvalidClient),
+			ErrorDescription: "Mismatching redirect_uri",
+		})
 		return
 	}
 
